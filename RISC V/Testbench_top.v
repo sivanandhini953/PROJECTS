582609b556f15f536614b4a43c6f43be7247a159
sivanandhini953 <sivanandhinis69@gmail.com>
2025-06-04 11:30:17 +0530

    Create Testbench_top.v

---
+++ RISC V/Testbench_top.v
@@ -0,0 +1,20 @@
+ 
+ module tb_top;
+ 
+ reg clk,reset;
+ 
+ top uut(.clk(clk),.reset(reset));
+ 
+ initial begin
+ clk=0;
+ reset=1;
+ #5;
+ reset=0;
+ 
+ #400 $finish;
+ 
+ end
+ 
+ always #5 clk=~clk;
+ 
+ endmodule
